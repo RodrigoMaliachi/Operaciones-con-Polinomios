2b01151277b6493de1964526d6a688b21229a2e0
Jonatan <a17000799@alumnos.uady.mx>
2022-02-21 19:33:47 -0600

    Agregué los métodos para sumar y multiplicar

---
+++ src/main/java/Modelo/ListaDoble.java
@@ -0,0 +1,167 @@
+package Modelo;
+
+/**
+ *
+ * @author Jonatan, Natali, Rodrigro, Angélica
+ */
+public class ListaDoble extends ListaD{
+    private int cantidadElementos;
+
+    
+    
+    public int getCantidadElementos() {
+        return cantidadElementos;
+    }
+
+    public void setCantidadElementos(int cantidadElementos) {
+        this.cantidadElementos = cantidadElementos;
+    }
+    
+    @Override
+    public void insertaInicio(Polinomio dato) {
+        if(vacio()){
+            inicio = ultimo = new NodoDoble(dato);
+            cantidadElementos++;
+        }else{
+            NodoDoble nuevo = new NodoDoble(dato,inicio,null);
+            inicio.setAnterior(nuevo);
+            inicio = nuevo;
+            cantidadElementos++;
+        }
+    }
+
+    @Override
+    public void insertarFinal(Polinomio dato) {
+        if(vacio()){
+            inicio = ultimo = new NodoDoble(dato);
+        }else{
+            NodoDoble nuevo = new NodoDoble(dato,null,ultimo);
+            ultimo.setSiguiente(nuevo);
+            ultimo = nuevo;
+        }
+    }
+    
+    public void InsertaAntes(Polinomio dato, int posicion){
+        NodoDoble actual = inicio;
+        int i=0;
+        while(actual != null && i != posicion){
+            actual = actual.siguiente;
+            i++;
+        }
+        NodoDoble nuevo = new NodoDoble(dato,actual,actual.getAnterior());
+        NodoDoble tempo = actual.getAnterior();
+        actual.anterior = nuevo;
+        tempo.setSiguiente(nuevo);
+    }
+    
+    public void InsertaDespues(Polinomio dato,int posicion){
+        NodoDoble actual = inicio;
+        int i=0;
+        while(actual != null && i != posicion){ 
+            actual = actual.siguiente;
+            i++;
+        }
+        NodoDoble nuevo = new NodoDoble(dato,actual.siguiente,actual);
+        NodoDoble tempo = actual.siguiente;
+        actual.siguiente = nuevo;
+        tempo.setAnterior(nuevo);
+    }
+    
+    @Override
+    public Object eliminaInicio() {
+        Object eliminado = null;
+        if(vacio()){
+             System.out.println("La lista está vacía");
+         }else{
+             if(inicio == ultimo){
+                 eliminado = inicio.getDato();
+                 inicio = ultimo = null;
+             }else{
+                 eliminado = inicio.getDato();
+                 inicio = inicio.getSiguiente();
+                 inicio.setAnterior(null);
+             }
+         }
+        return eliminado;
+    }
+
+    @Override
+    public Object eliminaFinal() {
+         Object eliminado = null;
+         if(vacio()){
+             System.out.println("La lista está vacía");
+         }else{
+             if(inicio == ultimo){
+                 eliminado = inicio.getDato();
+                 inicio = ultimo = null;
+             }else{
+                 eliminado = ultimo.getDato();
+                 ultimo = ultimo.getAnterior();
+                 ultimo.setSiguiente(null);
+             }
+         }
+         return eliminado;
+    }
+    
+    @Override
+    public void ordenarLista() {
+        Polinomio auxDato;
+        if(vacio()){
+            System.out.println("La lista está vacía");
+        }else{
+            if(inicio == ultimo){
+                System.out.println("La lista está ordenada");
+            }else{
+                NodoDoble actual = inicio;
+                while(actual != null){
+                    NodoDoble p = actual.getSiguiente();
+                    while(p != null){
+                        if(actual.getDato().getExponente() < p.getDato().getExponente()){
+                            auxDato = p.getDato();
+                            p.setDato(actual.getDato());
+                            actual.setDato(auxDato);
+                        }
+                        p = p.siguiente;
+                    }
+                    actual = actual.siguiente;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        ListaDoble lista = new ListaDoble();
+        lista.insertaInicio(new Polinomio(6,2));
+        lista.insertaInicio(new Polinomio(9,4));
+        lista.insertaInicio(new Polinomio(-3,5));
+        lista.insertaInicio(new Polinomio(2,6));
+        lista.insertaInicio(new Polinomio(4,1));
+        lista.insertaInicio(new Polinomio(15,0));
+        lista.insertaInicio(new Polinomio(-25,3));
+        lista.ordenarLista();
+
+        ListaDoble lista2 = new ListaDoble();
+        lista2.insertaInicio(new Polinomio(3,2));
+        lista2.insertaInicio(new Polinomio(2,6));
+        lista2.insertaInicio(new Polinomio(4,3));
+        lista2.insertaInicio(new Polinomio(4,5));
+        lista2.insertaInicio(new Polinomio(4,0));
+        lista2.insertaInicio(new Polinomio(9,1));
+        lista2.insertaInicio(new Polinomio(10,4));
+        lista2.ordenarLista();
+        System.out.println("pol1-----------------------------------");
+        lista.imprimir();
+        System.out.println("\nPol2-----------------------------------");
+        lista2.imprimir();
+        System.out.println("\nSuma-----------------------------------");
+        Polinomio pol1 = new Polinomio();
+        pol1.sumarPolinomios(lista, lista2,7);
+        System.out.println("\nMultiplicacion-----------------------------------");
+        pol1.multiplicarPolinomios(lista, lista2);
+        
+        
+    }
+
+
+    
+}
---
+++ src/main/java/Modelo/Polinomio.java
@@ -0,0 +1,143 @@
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jonatan, Natali, Rodrigro, Angélica
+ */
+
+public class Polinomio {
+    private int coeficiente;
+    private int exponente;
+
+    public Polinomio(){
+        this.coeficiente=0;
+        this.exponente=0;
+    }
+
+    public Polinomio(int coeficiente, int exponente){
+        this.coeficiente = coeficiente;
+        this.exponente = exponente;
+    }
+
+    public int getCoeficiente() {
+        return coeficiente;
+    }
+
+    public void setCoeficiente(int coeficiente) {
+        this.coeficiente = coeficiente;
+    }
+
+    public int getExponente() {
+        return exponente;
+    }
+
+    public void setExponente(int exponente) {
+        this.exponente = exponente;
+    }
+    
+    @Override
+    public String toString(){
+        if(exponente == 0){
+            return coeficiente+"";
+        }else{
+            return coeficiente+"x^"+exponente;
+        }
+        
+    }
+    
+    public ListaDoble sumarPolinomios(ListaDoble polinomioUno, ListaDoble polinomioDos,int n){
+        int [] coeficientePolinomioUno = new int [n];
+        int [] coeficientePolinomioDos = new int [n];
+        ListaDoble auxLista = new ListaDoble();
+        
+        for(int i= 0; i<n ;i++){
+            coeficientePolinomioDos[i] = 0;
+            coeficientePolinomioUno[i] = 0;
+        }
+        
+        NodoDoble actual = polinomioUno.inicio;
+        while(actual != null){
+            coeficientePolinomioUno[actual.getDato().getExponente()] = actual.getDato().getCoeficiente();
+            actual = actual.siguiente;
+        }
+        actual = polinomioDos.inicio;
+        while(actual != null){
+            coeficientePolinomioDos[actual.getDato().getExponente()] = actual.getDato().getCoeficiente();
+            actual = actual.siguiente;
+        }
+        
+        for(int i= 0; i<n ;i++){
+            int acumulador;
+            acumulador = coeficientePolinomioDos[i] + coeficientePolinomioUno[i];
+            if(acumulador != 0){
+                auxLista.insertaInicio(new Polinomio(acumulador,i));
+            }
+        }  
+        
+        auxLista.imprimir();  
+        return auxLista;
+    }
+    
+    public void multiplicarPolinomios(ListaDoble polinomioUno, ListaDoble polinomioDos){
+        NodoDoble poliUno, poliDos;
+        ListaDoble pol1 = new ListaDoble();
+        
+        poliUno = polinomioUno.inicio;
+        while(poliUno != null){
+            poliDos = polinomioDos.inicio;
+            while(poliDos != null){
+                int coeficien = poliUno.getDato().getCoeficiente()*poliDos.getDato().getCoeficiente();
+                int exponen = poliUno.getDato().getExponente()+poliDos.getDato().getExponente();
+                pol1.insertaInicio(new Polinomio(coeficien,exponen));
+                poliDos = poliDos.siguiente;
+            }
+            poliUno = poliUno.siguiente;
+        }
+        pol1.ordenarLista();
+        pol1.imprimir();
+        System.out.println("\nSimplificado-------------------------------");
+        SimplificarPolinomio(pol1,13);
+         
+    }
+    
+    public void SimplificarPolinomio(ListaDoble polinomioUno,int n){
+         NodoDoble poliUno;
+         ListaDoble pol1 = new ListaDoble();
+         int [] coeficientePolUno = new int [n];
+         
+         for(int i= 0; i<n ;i++){
+            coeficientePolUno[i] = 0;
+         }
+         
+        poliUno = polinomioUno.inicio;
+        while(poliUno != null){
+            coeficientePolUno[poliUno.getDato().getExponente()] = coeficientePolUno[poliUno.getDato().getExponente()] + poliUno.getDato().getCoeficiente();
+            poliUno = poliUno.siguiente;
+        }
+        
+        for(int i= 0; i<n ;i++){
+            if(coeficientePolUno[i] != 0){
+                pol1.insertaInicio(new Polinomio(coeficientePolUno[i],i));
+            }
+        }  
+         
+         pol1.imprimir();
+    }
+    
+    public boolean verificar(ListaDoble polinomioUno,int x){
+        boolean ver = false;
+        NodoDoble actual = polinomioUno.inicio;
+        while(actual != null){
+            if(actual.getDato().exponente == x){
+                ver = true;
+                actual = null;
+            }else{
+                actual = actual.getSiguiente();
+            }
+        }
+        return ver;
+    }
+
+}
